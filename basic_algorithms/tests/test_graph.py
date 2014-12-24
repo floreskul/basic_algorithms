3c3f70fca265bc0c0a5d2b2be4eab35ce5ab2ad3
Volodymyr Floreskul <volodymyr@floreskul.name>
2014-12-24 22:31:04 +0100

    Added more BFS and DFS tests

--- basic_algorithms/tests/test_graph.py
+++ basic_algorithms/tests/test_graph.py
@@ -5,27 +5,38 @@ from ..graph.dfs import dfs
 class TestGraphAlgorithms(unittest.TestCase):
     
     def setUp(self):
-        self.bfs_graph = {
-                          1: [2, 3],
-                          2: [1, 4, 5],
-                          3: [1],
-                          4: [2],
-                          5: [2]
-                          }
+        self.bfs_graph1 = {
+            1: [2, 3],
+            2: [1, 4, 5],
+            3: [1],
+            4: [2],
+            5: [2]
+        }
+        self.bfs_graph2 = {
+            'a': ['b', 'c'],
+            'b': ['d', 'e'],
+            'c': ['g'],
+            'd': ['f'],
+            'e': ['f'],
+            'f': [],
+            'g': []
+        }
         self.spanning_tree_graph = {
-                          'a': {'b': 1, 'd': 4, 'e': 3},
-                          'b': {'a': 1, 'd': 4, 'e': 2},
-                          'c': {'e': 4, 'f': 5},
-                          'd': {'a': 4, 'b': 4, 'e': 4},
-                          'e': {'a': 3, 'b': 4, 'd': 4, 'f': 7},
-                          'f': {'c': 5, 'e': 7}
-                          }
+            'a': {'b': 1, 'd': 4, 'e': 3},
+            'b': {'a': 1, 'd': 4, 'e': 2},
+            'c': {'e': 4, 'f': 5},
+            'd': {'a': 4, 'b': 4, 'e': 4},
+            'e': {'a': 3, 'b': 4, 'd': 4, 'f': 7},
+            'f': {'c': 5, 'e': 7}
+        }
     
     def test_bfs(self):
-        self.assertEqual(bfs(self.bfs_graph, 1), [1, 2, 3, 4, 5])
+        self.assertEqual(bfs(self.bfs_graph1, 1), [1, 2, 3, 4, 5])
+        self.assertEqual(bfs(self.bfs_graph2, 'a'), ['a', 'b', 'c', 'd', 'e', 'g', 'f'])
         
     def test_dfs(self):
-        self.assertEqual(dfs(self.bfs_graph, 1), [1, 3, 2, 5, 4])
+        self.assertEqual(dfs(self.bfs_graph1, 1), [1, 3, 2, 5, 4])
+        self.assertEqual(dfs(self.bfs_graph2, 'a'), ['a', 'c', 'g', 'b', 'e', 'f', 'd'])
     
     # def test_minimum_spanning_tree(self, algorithm):
     #     tree = algorithm(self.spanning_tree_graph)
