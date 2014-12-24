76f44343b1fdf777db55a424eb856a8be6927b29
Volodymyr Floreskul <volodymyr@floreskul.name>
2014-12-24 22:31:32 +0100

    Rewritten BFS to match description in Wikipeadia

--- basic_algorithms/graph/dfs.py
+++ basic_algorithms/graph/dfs.py
@@ -5,13 +5,13 @@
 
 def dfs(adjacent_nodes, start):
     node_stack = [start]
-    visited_nodes = set([start])
+    visited_nodes = set()
     path = []
     while node_stack:
         current_node = node_stack.pop()
-        path.append(current_node)
-        for node in adjacent_nodes[current_node]:
-            if node not in visited_nodes:
-                visited_nodes.add(node)
+        if current_node not in visited_nodes:
+            path.append(current_node)
+            visited_nodes.add(current_node)
+            for node in adjacent_nodes[current_node]:
                 node_stack.append(node)
     return path
