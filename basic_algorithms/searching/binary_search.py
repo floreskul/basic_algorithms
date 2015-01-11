d33811aaa9a6fffb30dcf23126cd75e8ca55a836
Volodymyr Floreskul <volodymyr@floreskul.name>
2015-01-11 22:06:43 +0100

    Removed unnecessary print statements

--- basic_algorithms/searching/binary_search.py
+++ basic_algorithms/searching/binary_search.py
@@ -5,12 +5,10 @@
 import random
 
 def binary_search(x, key):
-    print(x, key)
     left = 0
     right = len(x) - 1
     while left <= right:
         middle = (left + right) // 2
-        print (middle, left, right)
         if x[middle] < key:
             left = middle + 1
         elif x[middle] > key:
